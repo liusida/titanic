70eb7f4211b4767c8ac3d35ffedf4d5976664aaf
Sida Liu <me@liusida.com>
2019-12-05 23:24:42 -0500

    reading data from simulation

--- src/exp1.cpp
+++ src/exp1.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include <sim.h>
 
 int main() {
   Simulation sim;
 
-  Mass *a = sim.createMass(Vec(0,1,1));
+  Mass *a = sim.createMass(Vec(0,0,2));
   Mass *b = sim.createMass(Vec(0,0,1));
   sim.createSpring(a, b);
   sim.createPlane(Vec(0, 0, 1), 0); // create constraint plane
 
   sim.start();
-  std::cout<<"simulation started."<<std::endl;
+  std::cout<<"simulation Thread Started."<<std::endl;
+
+  while(1) {
+      sim.getAll();
+      std::cout<<"Reading a position: "<<a->pos<<std::endl;
+      std::cout<<"Reading b position: "<<b->pos<<std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
 }
